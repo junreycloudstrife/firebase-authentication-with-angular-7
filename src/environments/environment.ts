0ec15e440ff31aef8aa5fc21e13f6e64d7a752d3
junreycloudstrife <junrey.cloudstrife@gmail.com>
2019-09-26 10:54:20 +0800

    Change the firebase credentials

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,14 +3,27 @@
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
+// export const environment = {
+//   production: false,
+//   firebase: {
+//     apiKey: "AIzaSyBmNkRGMwyD_Z6sPYY3FtKI4H9w7kUzYi4",
+//     authDomain: "cohesive-apogee-95023.firebaseapp.com",
+//     databaseURL: "https://cohesive-apogee-95023.firebaseio.com",
+//     projectId: "cohesive-apogee-95023",
+//     storageBucket: "cohesive-apogee-95023.appspot.com",
+//     messagingSenderId: "546552200902"
+//   }
+// };
+
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyBmNkRGMwyD_Z6sPYY3FtKI4H9w7kUzYi4",
-    authDomain: "cohesive-apogee-95023.firebaseapp.com",
-    databaseURL: "https://cohesive-apogee-95023.firebaseio.com",
-    projectId: "cohesive-apogee-95023",
-    storageBucket: "cohesive-apogee-95023.appspot.com",
-    messagingSenderId: "546552200902"
+    apiKey: "AIzaSyAt4aZVi69s-6ET6B1LMGlK6l-lP1BGcbg",
+    authDomain: "trombi-generator.firebaseapp.com",
+    databaseURL: "https://trombi-generator.firebaseio.com",
+    projectId: "trombi-generator",
+    storageBucket: "trombi-generator.appspot.com",
+    messagingSenderId: "985945203195",
+    appId: "1:985945203195:web:ddac98105a20510c"
   }
 };
